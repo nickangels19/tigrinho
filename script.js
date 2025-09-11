da5e48a8f30ddeaabe4bfe85f1f5c78d03415138
nickangels19 <145124180+nickangels19@users.noreply.github.com>
2025-09-11 19:58:25 -0300

    ok

---
+++ script.js
@@ -0,0 +1,27 @@
+const caixaPrincipal=document.querySelector("caixa-principal");
+const caixaPerguntas=document.querySelector("caixa-perguntas");
+const caixaAlternativas=document.querySelector("caixa-alternativas");
+const caixaResultado=document.querySelector("caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
+
+
+const perguntas=[
+    {
+enunciado:"assim que saiu do colégio voce se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele tambem gera imagens e audios realistas. Qual e o seu pensamento?",
+alternativas:[
+    {
+        texto:"isso e assustador",
+        afirmacao:"no inicio, eu ficaria com medo do que essa tecnologia pode fazer."
+    },
+    {
+        texto:"isso e incrivel",
+        afirmacao: "sempre quis saber como usar uma IA em meu dia a dia."
+    }
+
+]
+    }
+
+]
+
+
+
